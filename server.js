dd65f962f18586eef0e0862266287a6b2ed82bb5
Wilfried Bour <wilfried.bourguignon@epitech.eu>
2026-09-23 15:05:24 +0200

    Step 2: Add express-session module

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const authRouter = require('./routes/auth');
-
+const dotenv = require('dotenv');
 const app = express();
+const session = require('express-session');
+
+dotenv.config();
 
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/auth', authRouter);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Serveur démarré sur http://localhost:${PORT}`);
-});
 
+const PORT = process.env.PORT || 3000;
+
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            httpOnly: true,
+            sameSite: 'strict',
+            maxAge: 1800000
+        }
+    })
+)
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`)
+});
